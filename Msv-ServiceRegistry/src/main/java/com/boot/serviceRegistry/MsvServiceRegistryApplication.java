c27fc7a40844203f6cf96d9ee3df9ccec0f1c6f1
shrikant.mosalgi <shrikant.mosalgi@snsl-868.skillnetinc.com>
2024-05-30 13:13:19 +0530

    host port independent

--- Msv-ServiceRegistry/src/main/java/com/boot/serviceRegistry/MsvServiceRegistryApplication.java
+++ Msv-ServiceRegistry/src/main/java/com/boot/serviceRegistry/MsvServiceRegistryApplication.java
@@ -1,6 +1,7 @@
 package com.boot.serviceRegistry;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
