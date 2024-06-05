bd7a42a05d2dabf45b950755af38b54c5947c115
shrikant.mosalgi <shrikant.mosalgi@snsl-868.skillnetinc.com>
2024-06-05 15:54:23 +0530

    fd

--- Msv-RatingsService/src/main/java/com/boot/rating/MsvRatingsServiceApplication.java
+++ Msv-RatingsService/src/main/java/com/boot/rating/MsvRatingsServiceApplication.java
@@ -2,8 +2,10 @@ package com.boot.rating;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+//@EnableFeignClients
 public class MsvRatingsServiceApplication {
 
 	public static void main(String[] args) {
--- Msv-RatingsService/src/main/java/com/boot/rating/ratingController/RatingController.java
+++ Msv-RatingsService/src/main/java/com/boot/rating/ratingController/RatingController.java
@@ -7,12 +7,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.boot.rating.entity.Rating;
 import com.boot.rating.service.RatingService;
 
 @RestController
+@RequestMapping("/rating")
 public class RatingController {
 
 	@Autowired
