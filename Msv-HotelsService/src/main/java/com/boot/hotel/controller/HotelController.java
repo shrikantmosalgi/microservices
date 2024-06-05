2090ddcfa1a3ec3bb26b41a9125395cb144d26b0
shrikant.mosalgi <shrikant.mosalgi@snsl-868.skillnetinc.com>
2024-06-05 15:55:41 +0530

    d

--- Msv-HotelsService/src/main/java/com/boot/hotel/controller/HotelController.java
+++ Msv-HotelsService/src/main/java/com/boot/hotel/controller/HotelController.java
@@ -7,12 +7,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.boot.hotel.entity.Hotel;
 import com.boot.hotel.service.HotelService;
 
 @RestController
+@RequestMapping("/hotel")
 public class HotelController {
 	
 	@Autowired
@@ -22,6 +24,7 @@ public class HotelController {
 	public Hotel createHotel(@RequestBody Hotel hotel){
 		return hotelService.createHotel(hotel);
 	}
+	
 	
 	@GetMapping("/getHotel/{id}")
 	public Hotel getHotel(@PathVariable int id) {
